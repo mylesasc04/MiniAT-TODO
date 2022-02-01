cd1aa060e57454aaf53f2a90dbf3cb22f48e01ff
Moreno Dizdarevic <moreno.dizdarevic@gmail.com>
2022-02-01 11:50:51 -0500

    Adding To-do

--- todo.java
+++ todo.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class todo {
 public static void main(String[] args) {
+    System.out.println("\u001b[33m-------------------    Welcome to Pepper   -------------------\u001b[37m");
+    System.out.println("\u001b[33mThe Go-To To-Do command line interface for software engineers!\n\u001b[37m");
+/*  
+    ANSI Color Codes:
+        Black: \u001b[30m
+        Red: \u001b[31m
+        Green: \u001b[32m
+        Yellow: \u001b[33m
+        Blue: \u001b[34m
+        Magenta: \u001b[35m
+        Cyan: \u001b[36m
+        White: \u001b[37m
+        Reset: \u001b[0m
+*/
 
     Scanner input = new Scanner(System.in);
 
@@ -13,23 +27,27 @@ public static void main(String[] args) {
     while (choice != 3) {
 
         System.out.println();
-        System.out.println("Type 1 to add a new thing to your to do list.");
-        System.out.println("Type 2 to print the to do list.");
-        System.out.println("Type 3 to exit the program.");
-        System.out.print("Select an option: ");
+        System.out.println("\u001b[33mTo view list of commands type 1\u001b[37m");
+        System.out.println("\u001b[33mOtherwise select option: \u001b[37m");
         choice = input.nextInt();
         int count = 0;
 
         if (choice == 1) {
-            System.out.println("Keep hitting enter after to do's, if you want to stop, type 'stop'.");
+            System.out.println("\u001b[33mCommands: \n - 1: View list of commands\n\u001b[37m");
+            System.out.println("\u001b[33m - 2: Add Task\n\u001b[37m");
+            System.out.println("\u001b[33m - 3: List Existing Tasks");
+        }
+
+        if (choice == 2) {
+            System.out.println("\u001b[33mAdd New Tasks, when done type done\n");
             for (int i=0;i<MAX;i++) {
                 list[i] = input.nextLine();
-                if (list[i].equals("stop")) break;
+                if (list[i].equals("done")) break;
                 count++;
             }
         }
 
-        if (choice == 2) {
+        if (choice == 3) {
             for (int index = 0;index < list.length; index++) {
                 System.out.println(list[index]);                    
             }               
