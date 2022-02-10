79cdb69f6ed55cdb4d0caaee99edf1df9be757e4
Nick <nick.mineo.nm@gmail.com>
2022-02-09 23:02:13 -0500

    Create pepper.bat

---
+++ todoProject/pepper.bat
@@ -0,0 +1,7 @@
+::Uncomment below to hide the commands
+::@echo off
+cd  "C:\Users\Myles\Downloads"
+:: cd  "C:\Users\%Myles%\Downloads"
+::Below is the line starts the .jar file, change it to your real one:
+javac printTodo.java
+java  printTodo.java
