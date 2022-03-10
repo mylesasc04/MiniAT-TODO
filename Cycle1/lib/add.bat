f6d982299bf3b12727c08d586120ebb6bedf9456
mylesasc04 <mylesduah123@gmail.com>
2022-03-09 22:05:11 -0500

    update

--- Cycle1/lib/add.bat
+++ Cycle1/lib/add.bat
@@ -1,12 +1,11 @@
 
 cd /d "MiniAT-TODO\Cycle1\lib"
 
-::javac -cp "./commons-lang3-3.12.0.jar; opencsv.jar;"
-::start javaw -commons-lang3-3.12.0.jar; 
-::start opencsv.jar 
 
-javac AddTodoTest.java
-java  AddTodoTest.java
+::commons-lang3-3.12.0.jar
+
+javac -cp  opencsv-5.6.jar;   AddTodoTest.java
+java  -cp  opencsv-5.6.jar; AddTodoTest.java
 
 cd
 cd  "C:\Users\Myles\Pepper"
